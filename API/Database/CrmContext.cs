9115735e15da585dd628c0f5c428380f0f2f5053
Micael Wollter <micael.wollter@gmail.com>
2023-11-03 15:34:39 +0100

    Add user

--- API/Database/CrmContext.cs
+++ API/Database/CrmContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using API.Database.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Database
 {
     public class CrmContext : DbContext
     {
         //entities
+        public DbSet<User> Users {get; set }
     }
 }
