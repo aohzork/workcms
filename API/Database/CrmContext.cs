985ab9817d3ba486715289fc222d7b66c06e59c4
Micael Wollter <micael.wollter@gmail.com>
2023-11-03 13:35:36 +0100

    basic dbcontext, local conn string setup

---
+++ API/Database/CrmContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Database
+{
+    public class CrmContext : DbContext
+    {
+        //entities
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder); 
+        }
+    }
+}
