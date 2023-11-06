84ddf5bff8180c5861a13dde7974cd68f04a3878
Micael Wollter <micael.wollter@gmail.com>
2023-11-06 13:50:12 +0100

    Add db creds

---
+++ API/Database/Models/UCred.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Database.Models
+{
+    public class UCred
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Password { get; set; }
+        public string Salt { get; set; }
+    }
+}
