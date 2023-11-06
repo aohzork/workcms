40309d6785f0c2f2053f126051a34da235f0e224
Micael Wollter <micael.wollter@gmail.com>
2023-11-06 11:50:55 +0100

    Frontend API configuration

---
+++ Site/Services/ApiSampleService.cs
@@ -0,0 +1,16 @@
+﻿using Site.Data;
+
+namespace Site.Services
+{
+    public class ApiSampleService
+    {
+        private readonly HttpClient _httpClient;
+        public ApiSampleService(HttpClient httpClient, ApiSettings apiSettings, IHostEnvironment environment)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri(environment.EnvironmentName.Equals("Development", StringComparison.OrdinalIgnoreCase) 
+                ? apiSettings.Development
+                : apiSettings.Production);
+        }
+    }
+}
