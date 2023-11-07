5839cc3ddacebad84ec6ee835a1441d55a33d96f
Micael Wollter <micael.wollter@gmail.com>
2023-11-07 12:07:31 +0100

    nuget and passwordmanager into backend

---
+++ API/System/PasswordManager.cs
@@ -0,0 +1,18 @@
+﻿namespace API.System
+{
+    public class PasswordManager
+    {
+        public static (string passwordHash, string salt) HashPassword(string password)
+        {
+            string salt = BCrypt.Net.BCrypt.GenerateSalt();
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword(salt, password);
+            return (passwordHash, salt);
+        }
+
+        public static bool VerifyPassword(string password, string storedPasswordHash, string salt)
+        {
+            string providedPasswordHash = BCrypt.Net.BCrypt.HashPassword(password, salt);
+            return (providedPasswordHash == storedPasswordHash);
+        }
+    }
+}
