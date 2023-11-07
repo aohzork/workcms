339c9be1b168f989594283c79d3826754c56e98b
Micael Wollter <micael.wollter@gmail.com>
2023-11-08 00:22:53 +0100

    dtos added

---
+++ API/DTOs/ApplicationLogDTO.cs
@@ -0,0 +1,12 @@
+﻿using API.Models;
+
+namespace API.DTOs
+{
+    public class ApplicationLogDTO
+    {
+        public int Id { get; set; }
+        public int ApplicationId { get; set; }
+        public string Message { get; set; }
+        public DateTime? Date { get; set; }
+    }
+}
