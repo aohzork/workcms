9c14996a50bdfafaf58414df01ef1cbe5932cfb3
Micael Wollter <micael.wollter@gmail.com>
2023-11-17 07:53:22 +0100

    Add DTO and service

---
+++ API/DTOs/JobApplicationUpdateRequestDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace API.DTOs
+{
+    public class JobApplicationUpdateRequestDTO
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Company {  get; set; }
+        public string Notes { get; set; }
+        public string ApplicationURL { get; set; }
+    }
+}
